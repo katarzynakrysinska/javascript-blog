587aa5b71154307b0faec773e223035f20284573
Kasia Krysińska <katarzyna.krysinska@onet.pl>
2021-08-11 23:05:24 +0200

    Add exercise 6.3

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
+'use strict';
+
 /* document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
   }); */
 
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
+    
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
+clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts .active');
 
 for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
 
-    /* add class 'active' to the correct article */
-  }
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+  
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+targetArticle.classList.add('active');
+
+}
   
 const links = document.querySelectorAll('.titles a');
   
